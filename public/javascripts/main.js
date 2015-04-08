6780b6d0bdad7766a8ee1e5502a90826104c17f5
Charimon <acharkin@gmail.com>
2015-04-08 13:38:27 -0700

    stuff

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -116,23 +116,13 @@
       }
     };
 
-    this.styles = [{
-      "elementType": "geometry",
-      "stylers": [ { "saturation": -100 } ]
-    },{
-      "featureType": "road.highway",
-      "stylers": [ { "visibility": "off" } ]
-    },{
-      "featureType": "road.arterial",
-      "stylers": [ { "visibility": "off" } ]
-    },{
-      "featureType": "poi",
-      "elementType": "labels",
-      "stylers": [ { "visibility": "off" } ]
-    },{
-      "featureType": "transit.line",
-      "stylers": [ { "visibility": "off" } ]
-    }];
+    this.styles = [
+      {"elementType": "geometry", "stylers": [ { "saturation": -100 } ]},
+      {"featureType": "road.highway", "stylers": [ { "visibility": "off" } ]},
+      {"featureType": "road.arterial","stylers": [ { "visibility": "off" } ]},
+      {"featureType": "poi","elementType": "labels","stylers": [ { "visibility": "off" } ]},
+      {"featureType": "transit.line", "stylers": [ { "visibility": "off" } ]}
+    ];
   }
   function InviteController($timeout, $routeParams, CompanyService, PersonService, NeighborhoodService){
     var self = this;
